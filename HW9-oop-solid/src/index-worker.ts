158ca93d4a97cc764eaf11a51f1cf74efc01ac97
andrii <apapmind@gmail.com>
2025-02-21 11:21:19 +0100

    Added index-worker.ts module

---
+++ HW9-oop-solid/src/index-worker.ts
@@ -0,0 +1,18 @@
+import { Engineer } from './engineer';
+import { Manager } from './manager';
+import { SoftwareEngineer } from './softwareEngineer';
+import { iEmployee } from './iemployee';
+
+function printWorkerDetails(worker: iEmployee): void {
+    console.log(worker.getSalary());
+    console.log(worker.getDescription());
+    console.log(worker.performDuties());
+}
+
+const engineer = new Engineer(true, false, 'Tom', 80000);
+const manager = new Manager(true, true, 'Bill', 120000);
+const softwareEngineer = new SoftwareEngineer(true, true, 'Gary', 100000, 'TypeScript');
+
+printWorkerDetails(engineer);
+printWorkerDetails(manager);
+printWorkerDetails(softwareEngineer);
